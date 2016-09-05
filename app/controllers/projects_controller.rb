88331f17ed98662738a316c71386c00f02b8a985
Sue <sbodily@att.net>
2016-09-04 22:47:08 -0600

    'Create a new project' feature complete.

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,30 @@
+class ProjectsController < ApplicationController
+
+  def index
+  end # index
+  
+  def new
+    @project = Project.new
+  end # new
+
+  def create
+    @project = Project.new(project_params)
+    
+    if @project.save
+      flash[:notice] = "Project has been created."
+      redirect_to @project
+    else
+      #nothing, yet
+    end # if
+  end # create
+  
+  def show
+    @project = Project.find(params[:id])
+  end # show
+  
+  private
+  
+  def project_params
+    params.require(:project).permit(:name, :description)
+  end # project_params
+end # ProjectsController class
