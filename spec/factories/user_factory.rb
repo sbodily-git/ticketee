4dee72b036fb500f1f53f7764680f58e8050472d
Sue <sbodily@att.net>
2016-09-09 19:46:45 -0600

    Added admin user and seede database with admin and non-admin user plus sample projects

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -2,5 +2,10 @@ FactoryGirl.define do
   factory :user do
     sequence(:email) { |n| "test#{n}@example.com" }
     password "password"
-  end
-end
+  
+    trait :admin do
+      admin true
+    end # trait block
+  end # user block
+
+end # define
