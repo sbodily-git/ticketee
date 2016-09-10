6fb89e27f0b2410c944450f780b37c203333476d
Sue <sbodily@att.net>
2016-09-09 23:21:57 -0600

    Added ability to set user as an admin during creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ActiveRecord::Base
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-end
+
+  def to_s
+    "#{email} (#{admin? ? "Admin" : "User"})"
+  end # to_s
+         
+end # User
