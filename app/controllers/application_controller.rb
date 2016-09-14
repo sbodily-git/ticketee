efb4c9863510b5c6b1bb56cfdaed4a150f0be7cc
Sue <sbodily@att.net>
2016-09-14 01:15:05 -0600

    Verify that we use authorization in all controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,11 @@
 class ApplicationController < ActionController::Base
   include Pundit
   
-  # Prevent CSRF attacks by raising an exception.
+  # Make sure that all actions are authorized
+  after_action :verify_authorized, except: [:index], unless: :devise_controller?
+  after_action :verify_policy_scoped, only: [:index], unless: :devise_controller?
+
+ # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
